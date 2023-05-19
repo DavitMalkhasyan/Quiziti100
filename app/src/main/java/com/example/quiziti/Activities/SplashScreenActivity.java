ed1283c7b5a0e675fada41d8d19d1634bfb87d2e
Davit <davitmalkhasyan16@gmail.com>
2023-05-19 23:03:43 +0400

    commit

---
+++ app/src/main/java/com/example/quiziti/Activities/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.quiziti.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.quiziti.MainActivity;
+import com.example.quiziti.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        },2000);
+    }
+}
